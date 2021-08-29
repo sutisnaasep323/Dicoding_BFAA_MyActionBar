606d1944abb0139f05d3bc9289bb945574247543
Asep Sutisna <sutisnaasep323@gmail.com>
2021-08-29 11:38:45 +0700

    Add comments

--- app/src/main/java/com/example/myactionbar/MainActivity.kt
+++ app/src/main/java/com/example/myactionbar/MainActivity.kt
@@ -14,19 +14,22 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater = menuInflater
-        inflater.inflate(R.menu.option_menu, menu)
+        inflater.inflate(R.menu.option_menu, menu) // menampilkan custom item pada action bar
         return true
     }
 
+    // memasang event listener untuk dijalankan ketika item tersebut dipilih. Listener click pada menu action bar dapat memanfaatkan onOptionsItemSelected()
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
+            // R.id.menu1 maka yang terjadi adalah fragment pada R.id.fragment_container diganti dengan fragment baru yaitu MenuFragment
             R.id.menu1 -> {
                 supportFragmentManager.beginTransaction()
                     .replace(R.id.fragment_container,MenuFragment())
-                    .addToBackStack(null)
+                    .addToBackStack(null) // menyimpan fragment
                     .commit()
                 return true
             }
+            // R.id.menu2 diklik, maka aplikasi akan menjalankan activity baru yaitu MenuActivity
             R.id.menu2 -> {
                 val i = Intent(this, MenuActivity::class.java)
                 startActivity(i)
